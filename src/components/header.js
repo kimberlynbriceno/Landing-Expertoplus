4d76ea96b0c425057f7b4a0d20c0ddf164bf6778
kimberlyn <kimberlynbriceno22@gmail.com>
2023-03-31 12:27:46 -0500

    style-home

--- src/components/header.js
+++ src/components/header.js
@@ -5,18 +5,18 @@ import Link from 'next/link'
 import { useState } from 'react';
 
 export default function header() {
-
   const [isMobile, setIsMobile] = useState(false);
 
   const handleMenu = () => {
+    
     isMobile ? setIsMobile(false) : setIsMobile(true);
   };
 
   return (
 
-    <header className="bg-header  flex items-center p-6 flex-wrap justify-between">
-      <div className="flex items-center lg:h-auto flex-shrink-0 mr-6">
-            <Image className=" h-22 w-32 md:ml-4 inline-block mt-0"
+    <header className="flex flex-wrap items-center justify-between p-6 bg-header">
+      <div className="flex items-center flex-shrink-0 mr-6 lg:h-auto">
+            <Image className="inline-block w-12 mt-0 h-22 md:ml-4"
              src={Logo.src} width={300} height={120} />
 
       </div>
@@ -25,10 +25,10 @@ export default function header() {
           {" "}
           <button
             onClick={handleMenu}
-            className="flex items-center px-2 py-2 rounded-2xl border bg-blue "
+            className="flex items-center px-2 py-2 border rounded-2xl bg-blue "
           >
             <img
-              className="w-5 h-5 block lg:inline-block lg:mt-0 items-center"
+              className="items-center block w-5 h-5 lg:inline-block lg:mt-0"
               src={MenuBurguer.src}
               alt="MenuBurguer"
             />
@@ -36,18 +36,18 @@ export default function header() {
         </div>
 
         <div
-          className={
-            isMobile
-              ? "w-full block lg:flex sm:items-center text-center lg:w-auto"
-              : "w-full lg:flex-grow lg:items-center lg:w-auto hidden"
-          }
+         className={
+          isMobile
+            ? "w-full block lg:flex sm:items-center text-center lg:w-auto"
+            : "w-full lg:flex lg:items-center lg:w-auto hidden"
+        }
         >
-        <nav className="lg:flex-grow lg:items-center m-3 text-primary pr-3 ">
+        <nav className="pr-3 m-3 lg:flex-grow lg:items-center text-primary ">
       
-            <Link className="pr-5 block mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 font-dfg" href="/"> Inicio</Link>
-            <Link className="pr-5 block mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 font-dfg" href="/Nosotros">Nosotros</Link>
-            <Link className="pr-5 block mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 font-dfg" href="/Servicios">Servicios</Link>
-            <Link className="bg-blue p-3 text-white block mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 rounded-3xl font-abc" href="/Contactanos">Contáctanos</Link>
+            <Link className="block pr-5 mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 font-dfg" href="/"> Inicio</Link>
+            <Link className="block pr-5 mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 font-dfg" href="/Nosotros">Nosotros</Link>
+            <Link className="block pr-5 mt-4 md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 font-dfg" href="/Servicios">Servicios</Link>
+            <Link className="block p-3 mt-4 text-white bg-blue md:ml-4 hover:text-slate-100 lg:inline-block lg:mt-0 rounded-3xl font-abc" href="https://web.whatsapp.com/">Contáctanos</Link>
 
         </nav>
        </div>
--- src/components/servicios.js
+++ src/components/servicios.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
 import Homen from '../img/imgHome.jpg';
+import Link  from 'next/link';
+import Wsslogo from '../img/wsslogo.svg'
+// import Nosotros from '../nosotros.js'
 
 
 export default function servicios() {
@@ -11,19 +14,71 @@ export default function servicios() {
         <div>
         <div className='w-full h-3/4'>
 
+        <div className='absolute flex flex-col items-start w-full h-full py-4 text-4xl space-y-7'> 
+                <p className='ml-8 text-white lg:text-8xl font-abc'>Transformas tus <br/> Proyectos en realidad <br/> con Expertoplus </p>
+                 <button className='p-3 ml-40 text-xl text-white bg-blue rounded-3xl font-abc'>Informacion</button>
+        
+            </div>
+            <div className='absolute flex flex-col items-end w-full h-full px-12 pt-72 md:pt-96 '><Link  href="https://web.whatsapp.com/"><Image  src={Wsslogo.src}
+                     width={80} height={80}/></Link></div>
+
             <div className='w-full h-full bg-black '>
                 <Image className='' src={Homen.src}
-                width={300} height={120}/>
+                width={600} height={600}/>
             </div>
 
-            <div className='w-full h-full flex flex-col absolute'>
-                <p className='bg-black text-white font-abc'>Transformas tus Proyectos en realidad con ExpertoPlus </p>
-                 <button className='bg-blue text-white p-3 rounded-xl'>Informacion</button>
-            </div>
+           
         </div>
         </div>
 
     </section>
+    <main className='flex flex-col items-center bg-gradient-to-r from-blue'>
+   {/* <div className='bg-white'> */}
+     <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="p-4 md:w-1/3">
+        <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+          <img className="object-cover object-center w-full lg:h-48 md:h-36" src="https://dummyimage.com/720x400" alt="blog"/>
+          <div className="p-6">
+            <h2 className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY</h2>
+            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">The Catalyzer</h1>
+            <p className="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <div className="flex flex-wrap items-center ">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+          <img className="object-cover object-center w-full lg:h-48 md:h-36" src="https://dummyimage.com/721x401" alt="blog"/>
+          <div className="p-6">
+            <h2 className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY</h2>
+            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">The 400 Blows</h1>
+            <p className="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <div className="flex flex-wrap items-center">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+          <img className="object-cover object-center w-full lg:h-48 md:h-36" src="https://dummyimage.com/722x402" alt="blog"/>
+          <div className="p-6">
+            <h2 className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font">CATEGORY</h2>
+            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Shooting Stars</h1>
+            <p className="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <div className="flex flex-wrap items-center ">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+{/* </div> */}
+    </main>
     
     </>
   )
